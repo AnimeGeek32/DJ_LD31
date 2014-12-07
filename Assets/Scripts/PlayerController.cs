be89eb85aae70c7da739f58488dcdba73c65a258
Daniel Jung <djanimelover18@gmail.com>
2014-12-07 04:01:57 -0500

    Added new assets. Started establishing core gameplay.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,102 @@
 ﻿using UnityEngine;
 using System.Collections;
+using InControl;
 
 public class PlayerController : MonoBehaviour {
+    public GameObject playerOnePaddle;
+    public GameObject playerTwoPaddle;
+
+    public float playerOneSpeed = 30.0f;
+    public float playerTwoSpeed = 30.0f;
+
+    private InputDevice playerOneDevice;
+    private InputDevice playerTwoDevice;
+    private bool playerOneLeftDown = false;
+    private bool playerOneRightDown = false;
+    private bool playerTwoLeftDown = false;
+    private bool playerTwoRightDown = false;
 
 	// Use this for initialization
 	void Start () {
-	
+        playerOneDevice = InputManager.Devices[0];
+        playerTwoDevice = InputManager.Devices[1];
+
+        playerOneLeftDown = false;
+        playerOneRightDown = false;
+        playerTwoLeftDown = false;
+        playerTwoRightDown = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        // Obtain gamepads' controls
+        Vector2 playerOneMovement = Vector2.zero;
+        Vector2 playerTwoMovement = Vector2.zero;
+
+        playerOneMovement.x = playerOneDevice.LeftStickX.Value * playerOneSpeed * Time.deltaTime;
+        playerTwoMovement.x = playerTwoDevice.LeftStickX.Value * playerTwoSpeed * Time.deltaTime;
+
+        // Key Ups
+        // Player One
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            playerOneLeftDown = false;
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+            playerOneRightDown = false;
+        }
+        // Player Two
+        if (Input.GetKeyUp(KeyCode.J))
+        {
+            playerTwoLeftDown = false;
+        }
+        else if (Input.GetKeyUp(KeyCode.L))
+        {
+            playerTwoRightDown = false;
+        }
+
+        // Key Downs
+        // Player One
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            playerOneLeftDown = true;
+            
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            playerOneRightDown = true;
+            
+        }
+        // Player Two
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            playerTwoLeftDown = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.L))
+        {
+            playerTwoRightDown = true;
+        }
+
+        // If there are keyboard inputs, then update movements
+        if (playerOneLeftDown)
+        {
+            playerOneMovement.x = -playerOneSpeed * Time.deltaTime;
+        }
+        else if (playerOneRightDown)
+        {
+            playerOneMovement.x = playerOneSpeed * Time.deltaTime;
+        }
+        if (playerTwoLeftDown)
+        {
+            playerTwoMovement.x = -playerTwoSpeed * Time.deltaTime;
+        }
+        else if (playerTwoRightDown)
+        {
+            playerTwoMovement.x = playerTwoSpeed * Time.deltaTime;
+        }
+
+        playerOnePaddle.rigidbody2D.velocity = playerOneMovement;
+        playerTwoPaddle.rigidbody2D.velocity = playerTwoMovement;
 	}
 }
---
+++ Assets/Scripts/SpermController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpermController : MonoBehaviour {
+    public float speed = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+        transform.rotation = Quaternion.Euler(0, 0, Random.Range(0f, 360.0f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rigidbody2D.velocity = transform.up * speed;
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "PlayerOne" || collision.collider.tag == "PlayerTwo")
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 180 + transform.rotation.eulerAngles.z);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 0, -transform.rotation.eulerAngles.z);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "P1Goal")
+        {
+            Debug.Log("Player One Wins!");
+            Respawn();
+        }
+        else if (collider.tag == "P2Goal")
+        {
+            Debug.Log("Player Two Wins!");
+            Respawn();
+        }
+    }
+
+    public void Respawn()
+    {
+        transform.position = Vector3.zero;
+        transform.rotation = Quaternion.Euler(0, 0, Random.Range(0f, 360.0f));
+    }
+}
